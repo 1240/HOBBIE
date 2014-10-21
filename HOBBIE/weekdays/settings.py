660937af72d475b12f00e948e1fa0beeeb803087
Alexander Popov <apopov@evelopers.com>
2014-10-21 12:10:41 +0400

    Рефактор + Структура проекта

--- HOBBIE/weekdays/settings.py
+++ HOBBIE/weekdays/settings.py
@@ -84,6 +84,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "room/static"),
+    os.path.join(BASE_DIR, "mainpage/static"),
+)
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
     os.path.join(BASE_DIR,  'room/templates'),
