436eca15f19cf3223e7931f7cbabebd5d014438f
Alexander Popov <apopov@evelopers.com>
2014-10-21 18:34:47 +0400

    карта /main

--- HOBBIE/room/urls.py
+++ HOBBIE/room/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^rooms/all/$', 'room.views.rooms'),
     url(r'^rooms/get/(?P<room_id>\d+)/$', 'room.views.room'),
     url(r'^rooms/addmessage/(?P<room_id>\d+)/$', 'room.views.addmessage'),
-    url(r'^$', 'room.views.rooms'),
+    url(r'^$', 'mainpage.views.home'),
 )
