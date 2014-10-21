41a91f698df4c985e8fc0d59427feb7482f3b4fa
Alexander Popov <apopov@evelopers.com>
2014-10-21 18:28:28 +0400

    карта /main

--- HOBBIE/mainpage/views.py
+++ HOBBIE/mainpage/views.py
@@ -6,8 +6,30 @@ from django.template import Context
 from django.template.loader import get_template
 
 
-def home(request):
+def home2(request):
     t = get_template('map.svg')
+    c = Context({
+    "width": 621,
+    })
+    svg = t.render(c)
+    r = HttpResponse(svg)
+    r['Content-Type'] = "image/svg+xml"
+    return HttpResponse(r)
+
+
+def home1(request):
+    t = get_template('test.html')
+    c = Context({
+    "width": 621,
+    })
+    svg = t.render(c)
+    r = HttpResponse(svg)
+    r['Content-Type'] = "image/svg+xml"
+    return HttpResponse(r)
+
+
+def home(request):
+    t = get_template('main_page.html')
     c = Context({
     "width": 621,
     })
