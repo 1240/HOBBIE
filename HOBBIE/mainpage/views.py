c00d6bab427f4ff0d504db37ce0c9d7afeb7c011
Alexander Popov <apopov@evelopers.com>
2014-10-21 18:07:44 +0400

    карта /main

--- HOBBIE/mainpage/views.py
+++ HOBBIE/mainpage/views.py
@@ -1,14 +1,17 @@
 import mimetypes
-import re
-from bs4 import BeautifulSoup
 from django.http import HttpResponse
 
 # Create your views here.
+from django.template import Context
 from django.template.loader import get_template
 
 
 def home(request):
-    view = "template"
-    template = get_template('main_page.html')
-    html = template
-    return HttpResponse(html)
+    t = get_template('map.svg')
+    c = Context({
+    "width": 621,
+    })
+    svg = t.render(c)
+    r = HttpResponse(svg)
+    r['Content-Type'] = "image/svg+xml"
+    return HttpResponse(r)
