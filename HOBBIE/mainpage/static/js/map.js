80071a864b19f4cebff454883a0337d605aebefe
1240 <chuffey.leto@gmail.com>
2014-10-26 09:28:17 +0400

    рефатор map + подсветка

--- HOBBIE/mainpage/static/js/map.js
+++ HOBBIE/mainpage/static/js/map.js
@@ -3,8 +3,19 @@ $(window).load(function () {
     var paths = svg.getElementsByTagName("path");
     var i;
     for (i=0; i<paths.length;i++) {
-        paths[i].addEventListener('click', showAlert )
-        $(paths[i]).mouseleave(HideTooltip)
+        paths[i].addEventListener('click', addLink );
+        $(paths[i]).mouseleave(HideTooltip);
+        $(paths[i]).mouseenter(linkHover);
+    }
+
+    function linkHover() {
+        var region_id = this.getAttribute('data-region');
+        var links = document.getElementsByClassName('col-4 b-cities')[0].getElementsByTagName('a')
+        for (i=0; i<links.length; i++) {
+            if ($(links[i]).attr('id').indexOf(region_id) > -1) {
+                $(links[i]).attr('data-state', 'hover');
+            }
+        }
     }
 
     function HideTooltip()
@@ -13,9 +24,16 @@ $(window).load(function () {
 	    var tooltip_bg = this.parentElement.getElementById('tooltip_bg');
 	    tooltip.setAttributeNS(null,"visibility","hidden");
 	    tooltip_bg.setAttributeNS(null,"visibility","hidden");
+        var region_id = this.getAttribute('data-region');
+        var links = document.getElementsByClassName('col-4 b-cities')[0].getElementsByTagName('a')
+        for (i=0; i<links.length; i++) {
+            if ($(links[i]).attr('id').indexOf(region_id) > -1) {
+                $(links[i]).attr('data-state', '');
+            }
+        }
 	}
 
-    function showAlert(e) {
+    function addLink(e) {
         var title = this.getAttribute("data-title");
         var id = this.getAttribute("data-region");
         var a_now = document.getElementById("region_" + id);
@@ -31,7 +49,6 @@ function addHover(region_id) {
     for (i=0; i<paths.length;i++) {
         if (region_id.indexOf(paths[i].getAttribute('data-region')) > -1) {
             $(paths[i]).attr('data-state', 'hover');
-            break;
         }
     }
 }
@@ -44,7 +61,6 @@ function deleteHover(region_id) {
     for (i=0; i<paths.length;i++) {
         if (region_id.indexOf(paths[i].getAttribute('data-region')) > -1) {
             $(paths[i]).attr('data-state', '');
-            break;
         }
     }
 }
