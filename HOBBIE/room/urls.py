b696b0269e21e249f4a30fa4c513b80f98da4bda
Alexander Popov <apopov@evelopers.com>
2014-10-29 14:14:25 +0300

    fix rooms urls

--- HOBBIE/room/urls.py
+++ HOBBIE/room/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url, patterns, include
 __author__ = '1240'
 
 urlpatterns = patterns('',
-    url(r'^rooms/(?P<region_name>\w+)/$', 'room.views.rooms'),
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     url(r'^rooms/get/(?P<room_id>\d+)/$', 'room.views.room'),
     #url(r'^rooms/addmessage/(?P<room_id>\d+)/$', 'room.views.addmessage'),
@@ -13,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^rooms/addroom/$', 'room.views.addroom'),
     url(r'^rooms/join/(?P<room_id>\d+)/$', 'room.views.joinroom'),
     url(r'^rooms/invite/(?P<room_id>\d+)/$', 'room.views.invite'),
+    url(r'^rooms/(?P<region_name>\w+)/$', 'room.views.rooms'),
+
 )
