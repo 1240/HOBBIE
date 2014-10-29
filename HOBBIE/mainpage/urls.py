a79fab8e81c300871b1484932f91d61364a8fe2b
Alexander Popov <apopov@evelopers.com>
2014-10-29 10:28:03 +0300

    mainpage

--- HOBBIE/mainpage/urls.py
+++ HOBBIE/mainpage/urls.py
@@ -4,4 +4,6 @@ __author__ = '1240'
 
 urlpatterns = patterns('',
     url(r'^$', 'mainpage.views.home', name="Главная"),
+    url(r'^main_west/', 'mainpage.views.main_west', name="Главная"),
+    url(r'^main_east/', 'mainpage.views.main_east', name="Главная"),
 )
--- HOBBIE/mainpage/views.py
+++ HOBBIE/mainpage/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
 # Create your views here.
+from django.shortcuts import redirect
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib import auth
@@ -8,14 +9,31 @@ from mainpage.models import Regions
 
 
 def home(request):
-    t = get_template('main_page.html')
+    if 'main_east' == request.COOKIES.get('world'):
+        regions = Regions.objects.filter(is_west=False)
+        t = get_template('main_east.html')
+    else:
+        regions = Regions.objects.filter(is_west=True)
+        t = get_template('main_west.html')
     c = Context({
         "width": 621,
         "username": auth.get_user(request).username,
-        "regions": Regions.objects.all(),
+        "regions": regions,
         "id_user": auth.get_user(request).id,
     })
     svg = t.render(c)
     r = HttpResponse(svg)
     r['Content-Type'] = "image/svg+xml"
     return HttpResponse(r)
+
+
+def main_east(request):
+    r = redirect('/')
+    r.set_cookie('world', 'main_east')
+    return r
+
+
+def main_west(request):
+    r = redirect('/')
+    r.set_cookie('world', 'main_west')
+    return r
