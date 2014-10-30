d3afc991f168c9934c397ff4180c23fdfd1af23c
Alexander Popov <apopov@evelopers.com>
2014-10-30 18:01:56 +0300

    main fucking page

--- HOBBIE/accounts/urls.py
+++ HOBBIE/accounts/urls.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from dajaxice.core import dajaxice_config
+
 _author__ = '1240'
 from django.conf.urls import patterns, include, url
 from django.conf.urls import url, patterns
@@ -8,5 +10,7 @@ __author__ = '1240'
 
 urlpatterns = patterns('',
     url(r'^edit/', 'accounts.views.edit'),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+
     url(r'^(?P<username>\w+)/', 'accounts.views.user_page'),
 )
--- HOBBIE/weekdays/settings.py
+++ HOBBIE/weekdays/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
