da1f0fc94615052e1c1ca5186c76620516b03c83
1240 <chuffey.leto@gmail.com>
2014-11-09 22:49:56 +0300

    filter by user_rooms

--- HOBBIE/accounts/views.py
+++ HOBBIE/accounts/views.py
@@ -54,10 +54,12 @@ def friends(request):
 
 def rooms(request):
     user = auth.get_user(request)
-    usinroom = []
+    '''usinroom = []
     for userroom in UserRoom.objects.filter(user_id=user.id, message_text__isnull=True):
         usinroom.append(userroom.room)
-    current_page = Paginator(object_list=usinroom, per_page=10)
+    current_page = Paginator(object_list=usinroom, per_page=10)'''
+    current_page = Paginator(object_list=user.room.all()
+                             .order_by("-room_create_date"), per_page=10)
     args = {}
     args['rooms'] = current_page.page(1)
     args['toggle'] = 'notchecked'
